e442cfc1cc33d43609b6d47346e22b30c137dd8d
Hiroshi Saito <hiroshi3110@gmail.com>
2013-02-22 14:26:53 +0900

    Initial Commit

---
+++ firebase_ios_sample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  firebase_ios_sample
+//
+//  Created by hiroshi on 2013/02/22.
+//  Copyright (c) 2013年 yakitara.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
